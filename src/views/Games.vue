63eadcc26c8411b4d6ac2a9d5e0384ab98042ca8
Josiah Aubin <josiahaubin@gmail.com>
2019-09-15 14:29:46 -0600

    Set up games as the new home route

---
+++ src/views/Games.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="games">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "games",
+  components: {}
+};
+</script>
